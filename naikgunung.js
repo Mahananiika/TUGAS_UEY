aa6a13ae05838ea792357e020a600445c0cfffb6
Mahananiika <pratiwiika79@gmail.com>
2020-07-04 06:32:56 +0700

    payment

--- naikgunung.js
+++ naikgunung.js
@@ -11,3 +11,16 @@ function validasi() {
         alert('Anda harus mengisi data dengan lengkap !');
     }
 }
+
+// function myFunction() {
+//     var 
+//     var x = document.getElementById("mySelect").options.length;
+//     document.getElementById("demo").innerHTML = "Found " + x + " options in the list.";
+// }
+
+var verify = confrm('are you sure?');
+ if(verify===true){
+     alert('press button sure');
+ }else{
+     alert('press button cancel');
+ }
